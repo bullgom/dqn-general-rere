b41df538d6dd9f17dead200b32e7bf481a4dd137
inyoung <bullgom0213@gmail.com>
2022-07-21 11:31:54 +0900

    Moved image making outside

--- environment/cartpole.py
+++ environment/cartpole.py
@@ -1,6 +1,6 @@
 from environment.environment import Environment
 import gym
-from mytypes import Size, State
+from mytypes import Size, State, ActionSpace, Action
 
 
 class CartPole(Environment):
@@ -9,36 +9,28 @@ class CartPole(Environment):
         super().__init__(*args, **kwargs)
         self.env = gym.make("CartPole-v1")
 
-    def step(self, action: int) -> tuple[State, float, bool]:
-        _, r, done, _ = self.env.step(action)
+    def state(self) -> State:
         img = self.env.render("rgb_array")
-
         for prep in self.preps:
             img = prep(img)
+        return img
 
+    def step(self, action: Action) -> tuple[State, float, bool]:
+        _, r, done, _ = self.env.step(action["move_direction"])
+        img = self.state()
         return img, r, done
 
-    def reset(self) -> None:
+    def reset(self) -> State:
         self.env.reset()
+        state = self.state()
+        return state
 
-    def original_state_size(self) -> tuple[int]:
+    def original_state_size(self) -> Size:
         self.reset()
         x = self.env.render("rgb_array")
         s = x.shape
         x = {"w": s[1], "h": s[0], "c": s[2]}
         return x
 
-    def output_size(self) -> Size:
-        return {"output": self.env.action_space.n}
-
-
-if __name__ == "__main__":
-    from preprocessing import ToTensor, Resize
-    preps = [
-        ToTensor(),
-        Resize({"w": 84, "h": 84})
-    ]
-    cp = CartPole(preps)
-    cp.reset()
-    state, r, done = cp.step(1)
-    a = 1
+    def action_space(self) -> ActionSpace:
+        return {"move_direction": self.env.action_space.n}
