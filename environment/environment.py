eff0173bd3ad34cce52ece6f132a03f6b7521a88
inyoung <bullgom0213@gmail.com>
2022-07-21 11:32:12 +0900

    reset returns initial state

--- environment/environment.py
+++ environment/environment.py
@@ -1,31 +1,26 @@
 from preprocessing import Preprocessing
 from abc import ABC, abstractmethod
 from typing import Any, Optional
-from mytypes import Size, State
+from mytypes import Size, State, ActionSpace, Action
+
 
 class Environment(ABC):
-    
+
     def __init__(self, preps: list[Preprocessing] = None) -> None:
-        # Preprocessings 
+        # Preprocessings
         self.preps = preps if preps else []
-    
+
     @abstractmethod
-    def step(self, action: int) -> tuple[State, float, bool]:
+    def step(self, action: Action) -> tuple[State, float, bool]:
         raise NotImplementedError
-    
+
     @abstractmethod
-    def reset(self) -> None:
+    def reset(self) -> State:
         raise NotImplementedError
 
     @abstractmethod
     def original_state_size(self) -> Size:
         raise NotImplementedError
-
-    def size(self) -> Size:
-        input_size = self.state_size()
-        output_size = self.output_size()
-        input_size.update(output_size)
-        return input_size
 
     def state_size(self) -> Size:
         size = self.original_state_size()
@@ -34,5 +29,5 @@ class Environment(ABC):
         return size
 
     @abstractmethod
-    def output_size(self) -> Size:
+    def action_space(self) -> ActionSpace:
         raise NotImplementedError
