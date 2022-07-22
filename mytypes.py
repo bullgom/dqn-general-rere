1c154c17e1012618c3c92b8a467261a6570f75c0
inyoung <bullgom0213@gmail.com>
2022-07-22 20:43:11 +0900

    Added SARS

--- mytypes.py
+++ mytypes.py
@@ -6,3 +6,10 @@ Action = dict[str, int]
 ActionSpace = dict[str, int]
 Q = dict[str, torch.FloatTensor]
 State = Any
+Reward = float
+
+SARS = tuple[State, Action, Reward, State]
+TupleListSARS = tuple[list[State],
+                      list[Action],
+                      list[Reward],
+                      list[State]]
